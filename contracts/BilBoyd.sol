6fe686a6cf5d475e8acb111c9f29931fb445c723
idarev <idarev@stud.ntnu.no>
2024-10-20 13:14:29 +0200

    my work

---
+++ contracts/BilBoyd.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+contract Bilboyd_CarLeasing{
+    // Struct to hold car details
+    struct Car {
+        string model;
+        string color;
+        uint256 yearOfMatriculation;
+        uint256 originalValue;
+    }
+
+}
