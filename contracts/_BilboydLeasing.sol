5c96edd9c6f084b714879ab3157f7136f8029139
anderszk <anderszk@stud.ntnu.no>
2024-10-20 13:49:56 +0200

    Constructor and struct for car ifnormation

--- contracts/_BilboydLeasing.sol
+++ contracts/_BilboydLeasing.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract BilboydCarLeasing{
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract BilBoydCarLeasing is ERC721, Ownable {
+
+    struct Car {
+        string model;
+        string color;
+        uint16 yearOfMatriculation;
+        uint256 originalValue;
+    }
+
+    mapping(uint256 => Car) public cars;
+
+    constructor() ERC721("BilBoydCar", "BBC") Ownable(msg.sender) {}
 
 }
