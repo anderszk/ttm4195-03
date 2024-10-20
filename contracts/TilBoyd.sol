92d6da2372e82fcc00b1d4bf8bd8d1175eb6ef87
Engum <thomasjengum@gmail.com>
2024-10-20 14:10:33 +0200

    1.s'dtaft

--- contracts/TilBoyd.sol
+++ contracts/TilBoyd.sol
@@ -2,4 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-import " @openzeppelin / contracts /token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Bilboyd_CarLeasing is ERC721, Ownable {
+
+    struct Car {
+        string model;
+        string color;
+        uint256 yearOfMatriculation;
+        uint256 originalValue;
+    }
+
+    mapping{uint256 => Car} private carDetails;
+
+    constructor() ERC721("BillyBoyCar", "BSAR") Ownable(msg.sender) {}
+}
+
+
+
